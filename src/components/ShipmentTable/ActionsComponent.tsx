76f912e8a144304603d937c76b5bcf80bfd54418
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 00:22:38 +0200

    feat: implemented Actions component for ShipmentTable

---
+++ src/components/ShipmentTable/ActionsComponent.tsx
@@ -0,0 +1,35 @@
+import { Box, Button } from "@mui/material";
+
+import SearchIcon from "@mui/icons-material/Search";
+import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
+
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/Store";
+import { deleteShipment } from "../../features/Shipment/ShipmentSlice";
+
+type ActionsProps = {
+  orderNo: string;
+};
+
+export const ActionsComponent = ({ orderNo }: ActionsProps) => {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleDelete = () => {
+        dispatch(deleteShipment(orderNo));
+    }
+
+    const handleViewDetails = () => {
+
+    }
+
+  return (
+    <Box>
+      <Button variant="text" onClick={handleViewDetails} sx={{ minWidth: "0", mr: 1 }}>
+        <SearchIcon />
+      </Button>
+      <Button variant="text" onClick={handleDelete} sx={{ minWidth: "0" }} color="error">
+        <ClearOutlinedIcon />
+      </Button>
+    </Box>
+  );
+};
