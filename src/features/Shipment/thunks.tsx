24e31f5bbcc65b2dbc5deaadf138ff6f2e2f7f6c
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-14 23:26:40 +0200

    feat: moved asyncthunks to separate file

---
+++ src/features/Shipment/thunks.tsx
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ShipmentProps } from "../../constants";
+import axios from "axios";
+
+export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return axios
+    .get(import.meta.env.VITE_API_ENDPOINT)
+    .then((response) => response.data.map((order: ShipmentProps) => order));
+});
