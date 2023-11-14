bf5968f0d22b766ed4094b2cfb4eb993b4f8c7b1
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 00:21:34 +0200

    style: datagrid style

--- src/components/ShipmentTable/ShipmentTable.tsx
+++ src/components/ShipmentTable/ShipmentTable.tsx
@@ -8,18 +8,18 @@ type ShipmentTableProps = {
 
 export const ShipmentTable = ({ rows }: ShipmentTableProps) => {
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: "100%", width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={ShipmentTableColumns}
         getRowId={(row) => row.orderNo}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
+        pageSizeOptions={[10, 25, 50, 100]}
+        disableRowSelectionOnClick
       />
     </div>
   );
