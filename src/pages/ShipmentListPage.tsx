416c48de38448d64295e72505094b7557eab13ac
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 00:50:58 +0200

    feat: implemented shipment details component

--- src/pages/ShipmentListPage.tsx
+++ src/pages/ShipmentListPage.tsx
@@ -3,12 +3,16 @@ import { useSelector } from "react-redux";
 import { RootState } from "../app/Store";
 
 import { Container } from "@mui/material/";
+import { ShipmentDetails } from "../components/ShipmentDetails";
 
 export const ShipmentListPage = () => {
   const shipment = useSelector((state: RootState) => state.shipment);
   return (
-    <Container maxWidth={false}>
-      <ShipmentTable rows={shipment.shipments ?? []} />
-    </Container>
+    <>
+      <ShipmentDetails />
+      <Container maxWidth={false}>
+        <ShipmentTable rows={shipment.shipments ?? []} />
+      </Container>
+    </>
   );
 };
