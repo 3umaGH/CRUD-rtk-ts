06e3e466940fe224967440428fd6172a62a0b62e
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 00:23:25 +0200

    feat: load shipments on start up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "./app/Store";
+import { fetchUsers } from "./features/Shipment/thunks";
+import { ShipmentListPage } from "./pages/ShipmentListPage";
+
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShipmentListPage />
     </>
   );
 }
