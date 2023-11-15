6928acc00349d1477f9d10fa16187eb10792e9aa
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 13:31:42 +0200

    feat: added local data warning, style updates

--- src/pages/ShipmentListPage.tsx
+++ src/pages/ShipmentListPage.tsx
@@ -2,16 +2,41 @@ import { ShipmentTable } from "../components/ShipmentTable/ShipmentTable";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/Store";
 
-import { Container } from "@mui/material/";
+import { Container, CircularProgress, Alert } from "@mui/material/";
 import { ShipmentDetails } from "../components/ShipmentDetails";
 
 export const ShipmentListPage = () => {
   const shipment = useSelector((state: RootState) => state.shipment);
   return (
     <>
-      <ShipmentDetails />
-      <Container maxWidth={false}>
-        <ShipmentTable rows={shipment.shipments ?? []} />
+      <Container
+        maxWidth={false}
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ width: "100%" }}>
+          <ShipmentDetails />
+
+          {shipment.loading ? (
+            <CircularProgress size={"7.5rem"} />
+          ) : (
+            <>
+              {shipment.error && shipment.usingLocalData && (
+                <Alert severity="warning">
+                  Something went wrong with retreiving latest data. Using local
+                  copy.
+                </Alert>
+              )}
+
+              <ShipmentTable rows={shipment.shipments} />
+            </>
+          )}
+        </div>
       </Container>
     </>
   );
