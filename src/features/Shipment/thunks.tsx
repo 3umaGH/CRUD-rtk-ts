dc82404ab335e591b52ab5ce056cdb5548e3ac54
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-15 13:02:49 +0200

    fix: removed useless .map

--- src/features/Shipment/thunks.tsx
+++ src/features/Shipment/thunks.tsx
@@ -1,9 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { ShipmentProps } from "../../constants";
 import axios from "axios";
 
 export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
   return axios
     .get(import.meta.env.VITE_API_ENDPOINT)
-    .then((response) => response.data.map((order: ShipmentProps) => order));
+    .then((response) => response.data);
 });
